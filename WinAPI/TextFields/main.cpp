fd7e001a3ca8d62a4fdb467423629346fd331eef
IvchenkoDimon <87kinglion87@gmail.com>
2020-04-14 20:40:47 +0300

    WM_GET/SET_TEXT

--- WinAPI/TextFields/main.cpp
+++ WinAPI/TextFields/main.cpp
@@ -1,10 +1,18 @@
 #include<Windows.h>
 #include"resource.h"
 
+HWND hEdit1;
+HWND hEdit2;
+
+CHAR str1[] = "Hello guys !!!";
+CHAR str2[] = { 0 };
+
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
 	return 0;
 }
@@ -17,11 +25,22 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, LPARAM(hIcon));
+
+		hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
+		hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
+
+		SendMessage(hEdit1, WM_SETTEXT, 0, LPARAM(str1));
+
+		SetFocus(GetDlgItem(hwnd, IDC_BUTTON1));
 	}
 	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON1:
+			SendMessage(hEdit1, WM_GETTEXT, 255, LPARAM(str2));
+			SendMessage(hEdit2, WM_SETTEXT, 255, LPARAM(str2));
+			break;
 		case IDOK:
 			MessageBox(hwnd, "Hello", "Hi", MB_OK);
 				break;
