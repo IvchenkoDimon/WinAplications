582716b0f00d01391d9d09a8c08d8c1f19d16259
IvchenkoDimon <87kinglion87@gmail.com>
2020-04-14 20:47:08 +0300

    GET/SETWindowText

--- WinAPI/TextFields/main.cpp
+++ WinAPI/TextFields/main.cpp
@@ -29,8 +29,8 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		hEdit1 = GetDlgItem(hwnd, IDC_EDIT1);
 		hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
 
-		SendMessage(hEdit1, WM_SETTEXT, 0, LPARAM(str1));
-
+		//SendMessage(hEdit1, WM_SETTEXT, 0, LPARAM(str1));
+		SetWindowText(hEdit1, str1);
 		SetFocus(GetDlgItem(hwnd, IDC_BUTTON1));
 	}
 	break;
@@ -38,8 +38,10 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		switch (LOWORD(wParam))
 		{
 		case IDC_BUTTON1:
-			SendMessage(hEdit1, WM_GETTEXT, 255, LPARAM(str2));
-			SendMessage(hEdit2, WM_SETTEXT, 255, LPARAM(str2));
+			/*SendMessage(hEdit1, WM_GETTEXT, 255, LPARAM(str2));
+			SendMessage(hEdit2, WM_SETTEXT, 255, LPARAM(str2));*/
+			GetWindowText(hEdit1, str2, 255);
+			SetWindowText(hEdit2, str2);
 			break;
 		case IDOK:
 			MessageBox(hwnd, "Hello", "Hi", MB_OK);
