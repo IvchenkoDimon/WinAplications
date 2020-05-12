65af09628272b92bb05a3a537bc2d03f69317571
IvchenkoDimon <87kinglion87@gmail.com>
2020-05-12 18:11:59 +0300

    adsgf

---
+++ WinAPI/SimpleWindow/resource1.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDR_MENU1                       101
+#define ID_FILE_NEW                     40001
+#define ID_FILE_OPEN                    40002
+#define ID_FILE_SAVE                    40003
+#define ID_FILE_SAVEAS                  40004
+#define ID_FILE_EXIT                    40005
+#define ID_Menu                         40006
+#define ID_FILE_EXIT40007               40007
+#define ID_NEW_FILE                     40008
+#define ID_OPEN_FILE                    40009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
