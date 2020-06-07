2f9f1ee27425f06caaa80a5c4016da4da67c3f10
IvchenkoDimon <87kinglion87@gmail.com>
2020-06-07 18:21:55 +0300

    Text Color

---
+++ WinAPI/TextColor_/main.cpp
@@ -0,0 +1,35 @@
+#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProg, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, IDCANCEL);
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
