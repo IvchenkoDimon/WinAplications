958394aa7905bfd7e18d6b609a1c77d08fb11d95
IvchenkoDimon <87kinglion87@gmail.com>
2020-06-07 19:03:50 +0300

    Text Colorized

--- WinAPI/TextColor_/main.cpp
+++ WinAPI/TextColor_/main.cpp
@@ -14,7 +14,33 @@ BOOL CALLBACK DlgProg(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
+	{
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+		SetFocus(hEdit);
+	}
 		break;
+	case WM_CTLCOLORSTATIC:
+	{
+		
+		if ((HWND)lParam == GetDlgItem(hwnd, IDC_COLOR))
+		{
+			HDC hdc = (HDC)wParam;// HDC - Handler Device Context, 
+			SetBkMode(hdc, TRANSPARENT);
+			SetTextColor(hdc, RGB(0, 0, 200));
+			return (int)GetStockObject(NULL_BRUSH);
+		}
+	}
+	break;
+	case WM_CTLCOLOREDIT:
+	{
+		HDC hdc = (HDC)wParam;
+		SetBkColor(hdc, OPAQUE);
+		SetBkColor(hdc, RGB(100,100,100));
+		HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 200));
+		SetTextColor(hdc, RGB(0, 255, 0));
+		return (LRESULT)hBrush;
+	}
+	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
